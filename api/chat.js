281dc3e49e3be9e26e59c8fa7653a1ab932da6cd
mamiqmn-prog <mamiqmn@gmail.com>
2026-05-09 20:25:15 +0300

    Change export to CommonJS module syntax

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,7 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
+  // ... kodunun geri kalanı aynı kalabilir
+}
+
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
